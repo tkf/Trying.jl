2f55dfd9973cec80a78277713f7d3d24e10c17f2
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-30 18:15:33 -0700

    Initial implementation

--- README.md
+++ README.md
@@ -5,3 +5,31 @@
 [![Build Status](https://travis-ci.com/tkf/Trying.jl.svg?branch=master)](https://travis-ci.com/tkf/Trying.jl)
 [![Codecov](https://codecov.io/gh/tkf/Trying.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/tkf/Trying.jl)
 [![Coveralls](https://coveralls.io/repos/github/tkf/Trying.jl/badge.svg?branch=master)](https://coveralls.io/github/tkf/Trying.jl?branch=master)
+
+## Example
+
+```julia
+julia> using Trying
+
+julia> @try ones(1, 1) / zeros(1, 1)
+[ Info: Error thrown is available at `Trying.ERRORS[end]`.
+ERROR: LinearAlgebra.SingularException(1)
+Stacktrace:
+ [1] ldiv!(::LinearAlgebra.Diagonal{Float64,Array{Float64,1}}, ::Array{Float64,2}) at /.../LinearAlgebra/src/diagonal.jl:569
+ [2] ...
+
+julia> Trying.ERRORS
+1-element Array{Trying.ErrorRecord,1}:
+ ones(1, 1) / zeros(1, 1) threw SingularException
+
+julia> Trying.ERRORS[end]
+ErrorRecord
+Trying to evaluate expression:
+ones(1, 1) / zeros(1, 1)
+
+Error thrown:
+LinearAlgebra.SingularException(1)
+Stacktrace:
+ [1] ldiv!(::LinearAlgebra.Diagonal{Float64,Array{Float64,1}}, ::Array{Float64,2}) at /.../LinearAlgebra/src/diagonal.jl:569
+ [2] ...
+```
--- src/Trying.jl
+++ src/Trying.jl
@@ -1,5 +1,95 @@
 module Trying
 
-greet() = print("Hello World!")
+export @try
+
+"""
+    @try expression
+
+Evaluate `expression` and return the result.  If it throws an error,
+record the exception and backtrace in `Trying.ERRORS`.
+"""
+:(@try)
+
+using Parameters
+
+struct ErrorRecord
+    expression
+    exception
+    backtrace
+end
+
+function Base.show(io::IO, rec::ErrorRecord)
+    if get(io, :limit, false)
+        printstyled(io, rec.expression; color=:blue)
+        print(io, " threw ")
+        err = sprint(
+            summary,
+            rec.exception,
+            context = IOContext(io, :module => parentmodule(typeof(rec.exception))),
+        )
+        printstyled(io, err; color=:red)
+    else
+        invoke(show, Tuple{IO, Any}, io, rec)
+    end
+    return
+end
+
+function Base.show(io::IO, ::MIME"text/plain", rec::ErrorRecord)
+    println(io, "ErrorRecord")
+    printstyled(io, "Trying to evaluate expression:"; color=:blue)
+    println(io)
+    println(io, rec.expression)
+    println(io)
+    printstyled(io, "Error thrown:"; color=:blue)
+    println(io)
+    showerror(io, rec.exception, rec.backtrace)
+end
+
+"""
+    Trying.ERRORS :: Vector{ErrorRecord}
+
+Record of last `Trying.CONFIG.max_errors` errors.
+"""
+const ERRORS = ErrorRecord[]
+
+@with_kw mutable struct TryingConfig
+    max_errors::Int = 10
+end
+
+"""
+    Trying.CONFIG
+
+# Properties
+- `max_errors::Int = $(TryingConfig().max_errors)`:
+  Number of errors to be stored in `Trying.ERRORS`.
+"""
+const CONFIG = TryingConfig()
+
+function resize_errors()
+    inds = 1:length(ERRORS) - CONFIG.max_errors - 1
+    if !isempty(inds)
+        deleteat!(ERRORS, inds)
+    end
+    return
+end
+
+record_error(args...) = push!(ERRORS, ErrorRecord(args...))
+
+function try_impl(ex)
+    quote
+        resize_errors()
+        try
+            $(esc(ex))
+        catch exception
+            record_error($(QuoteNode(ex)), exception, catch_backtrace())
+            @info "Error thrown is available at `Trying.ERRORS[end]`."
+            rethrow()
+        end
+    end
+end
+
+@eval macro $(:try)(ex)
+    try_impl(ex)
+end
 
 end # module
